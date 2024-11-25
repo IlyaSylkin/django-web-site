350600e80df410c5e163515f3555fb1653cfe573
IlyaSylkin <ilyasylkin@outlook.com>
2024-11-26 02:28:33 +0300

    added css styles for all templates

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -5,12 +5,26 @@ from django.contrib.auth.forms import AuthenticationForm
 from .models import Profile
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=False)
-    last_name = forms.CharField(max_length=30, required=False)
-
+    first_name = forms.CharField(max_length=30, required=False, help_text='', label="Имя")
+    last_name = forms.CharField(max_length=30, required=False, help_text='', label="Фамилия")
+    password1 = forms.CharField(
+        label="Пароль",
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text='',  # Убираем help_text для пароля
+    )
+    password2 = forms.CharField(
+        label="Подтверждение пароля",
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text='',  # Убираем help_text для подтверждения пароля
+    )
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        help_texts = {
+            'username': '',  # Убираем подсказки для имени пользователя
+        }
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
